cdf6f8a3fa26c90435d2acffd226c565f2ce210f
AMEER JASSER BRIMA (OPS-IRIS-ISD-OOCLL/MNL) <BRIMAAM@OOCL.COM>
2021-08-05 17:51:56 +0800

    [MarsRover][AmeerBrima] feat: implemented solution should_return_locationX_0_locationY_1_direction_N_when_execute_command_given_locationX_0_locationY_direction_N_and_M test case.

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -10,6 +10,15 @@ public class MarsRover {
     }
 
     public void executeCommand(String command) {
+        if(command.equals("M")){
+            moveRover();
+        }
+    }
 
+    private void moveRover() {
+        int x = this.status.getLocationX();
+        int y = this.status.getLocationY();
+        String direction = this.status.getDirection();
+        this.status = new RoverStatus(x,y + 1,direction);
     }
 }
