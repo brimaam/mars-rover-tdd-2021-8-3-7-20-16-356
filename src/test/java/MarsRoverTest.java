4e7d9cdc0cca4bbe40527da5732580f99a008aad
AMEER JASSER BRIMA (OPS-IRIS-ISD-OOCLL/MNL) <BRIMAAM@OOCL.COM>
2021-08-05 19:11:38 +0800

    [MarsRover][AmeerBrima] test: added should_return_locationX_0_locationY_negative_1_direction_S_when_execute_command_given_locationX_0_locationY_0_direction_S_and_M test case.

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -47,4 +47,19 @@ public class MarsRoverTest {
         assertEquals(expectedRoverStatus.getDirection(), marsRover.getStatus().getDirection());
     }
 
+    @Test
+    void should_return_locationX_0_locationY_negative_1_direction_S_when_execute_command_given_locationX_0_locationY_0_direction_S_and_M() {
+        //given
+        MarsRover marsRover = new MarsRover(new RoverStatus(0,0,"S"));
+        RoverStatus expectedRoverStatus = new RoverStatus(0,-1,"S");
+
+        //when
+        marsRover.executeCommand("M");
+
+        //then
+        assertEquals(expectedRoverStatus.getLocationX(), marsRover.getStatus().getLocationX());
+        assertEquals(expectedRoverStatus.getLocationY(), marsRover.getStatus().getLocationY());
+        assertEquals(expectedRoverStatus.getDirection(), marsRover.getStatus().getDirection());
+    }
+
 }
